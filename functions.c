455c5f2bf0c6b4902a79dc2977f5beb1c34baf90
coder-vishnu98 <132753187+coder-vishnu98@users.noreply.github.com>
2023-05-06 16:53:43 +0530

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a[10];
+    int i,j,temp;
+    for ( i = 0; i < 10; i++)
+    {
+        cin>>a[i];
+    }
+    for ( i = 0; i < 9; i++)
+    {
+        for ( j = i+1; j < 10 ; j++)
+        {
+            if(a[i] > a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                
+            }
+        }
+        
+    }
+    for ( i = 0; i < 10; i++)
+    {
+        cout<<","<<a[i];
+    }
+    return 0;
+    }
